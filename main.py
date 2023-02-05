d483b7a7f273a491b25fc1adb0cd769ea1fe5293
VarshaVidya <111826112+VarshaVidya@users.noreply.github.com>
2023-02-05 19:28:52 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#TODO 1. Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+def generate_phonetic():
+    #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Enter a valid word")
+        generate_phonetic()
+    else:
+        print(output_list)
+generate_phonetic()
